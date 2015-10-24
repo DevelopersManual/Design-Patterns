3f128d2bda9c7efca0077603d2a27004fb796ee3
DevelopersManual <developersmanual@gmail.com>
2015-10-24 08:31:40 +0530

    packaged properly

---
+++ Singleton/src/com/developersmanual/dp/creational/singleton/EagerKing.java
@@ -0,0 +1,20 @@
+package com.developersmanual.dp.creational.singleton;
+
+
+/**
+ * @author admin
+ *
+ *Eager King creates the King object at the time of king object creation. its like selecting the king before 
+ *Kingdom got created.
+ */
+public class EagerKing {
+
+	private static EagerKing king = new EagerKing();
+	
+	private EagerKing() {
+	}
+	
+	public static EagerKing getInstance() {
+		return king;
+	}
+}
